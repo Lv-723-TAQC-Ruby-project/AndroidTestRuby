71e4dfeac0f85cade57b47f0058f5f2a1d569fec
simpleUA <a.lipskiy@yahoo.com>
2023-02-01 12:57:13 +0300

    fixed "visit www.google.com" test

--- visit_google_site_spec.rb
+++ visit_google_site_spec.rb
@@ -4,16 +4,21 @@ describe 'Visit www.google.com' do
   let(:google_site) { 'https://www.google.com' }
   let(:id_terms_accept) { 'com.android.chrome:id/terms_accept' }
   let(:id_negative_button) { 'com.android.chrome:id/negative_button' }
-  let(:url_bar) { 'com.android.chrome:id/url_bar' }
+  let(:id_url_bar) { 'com.android.chrome:id/url_bar' }
+  let(:xpath_logo) do
+    '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View[1]/android.view.View[3]/android.view.View[1]/android.widget.Image'
+  end
 
-  it 'should create and visit www.google.com' do
+
+
+  it 'should visit www.google.com' do
     caps = android_caps
     @driver = Appium::Driver.new(caps, true).start_driver
     @driver.find_element(:id, id_terms_accept).click
     @driver.find_element(:id, id_negative_button).click
     @driver.get(google_site)
-    @page_title = @driver.find_element(:id, url_bar).text
-    expect(@page_title).to eql 'https://www.google.com'
+    @page_logo = @driver.find_element(:xpath, xpath_logo).text
+    expect(@page_logo).to eql 'Google'
     @driver.quit
   end
 end
